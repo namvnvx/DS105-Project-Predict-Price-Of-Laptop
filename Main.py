347ba8aa23d5fe0a3382e32fb0f6ed990e8392ce
VNxVTTr <22521573@gm.uit.edu.vn>
2024-12-16 08:20:25 +0700

    First Commit

---
+++ Main.py
@@ -0,0 +1,4 @@
+from Source import FrontEnd
+
+if __name__ == "__main__":
+    FrontEnd.Main()
---
+++ Source/FrontEnd.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+
+
+######################## Main ##########################
+def Main():
+    st.title('Dự Đoán Giá Laptop 2')
